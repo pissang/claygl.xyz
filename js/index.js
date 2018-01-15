bc39af5142d61ab332772d8fe627683016aee69b
pissang <bm2736892@gmail.com>
2018-01-15 19:38:30 +0800

    Update index

---
+++ js/index.js
@@ -0,0 +1,162 @@
+clay.Shader.import(`@export clay.ground.vertex
+@import clay.lambert.vertex
+@end
+
+
+@export clay.ground.fragment
+
+varying vec2 v_Texcoord;
+varying vec3 v_Normal;
+varying vec3 v_WorldPosition;
+
+uniform vec4 color : [1.0, 1.0, 1.0, 1.0];
+uniform float gridSize: 5;
+uniform float gridSize2: 1;
+uniform vec4 gridColor: [0, 0, 0, 1];
+uniform vec4 gridColor2: [0.3, 0.3, 0.3, 1];
+
+uniform float glossiness: 0.7;
+
+#ifdef SSAOMAP_ENABLED
+// For ssao prepass
+uniform sampler2D ssaoMap;
+uniform vec4 viewport : VIEWPORT;
+#endif
+
+#ifdef AMBIENT_LIGHT_COUNT
+@import clay.header.ambient_light
+#endif
+#ifdef AMBIENT_SH_LIGHT_COUNT
+@import clay.header.ambient_sh_light
+#endif
+#ifdef DIRECTIONAL_LIGHT_COUNT
+@import clay.header.directional_light
+#endif
+
+@import clay.plugin.compute_shadow_map
+
+void main()
+{
+    gl_FragColor = color;
+
+    float wx = v_WorldPosition.x;
+    float wz = v_WorldPosition.z;
+    float x0 = abs(fract(wx / gridSize - 0.5) - 0.5) / fwidth(wx) * gridSize / 2.0;
+    float z0 = abs(fract(wz / gridSize - 0.5) - 0.5) / fwidth(wz) * gridSize / 2.0;
+
+    float x1 = abs(fract(wx / gridSize2 - 0.5) - 0.5) / fwidth(wx) * gridSize2 / 2.0;
+    float z1 = abs(fract(wz / gridSize2 - 0.5) - 0.5) / fwidth(wz) * gridSize2 / 2.0;
+
+    float v0 = 1.0 - clamp(min(x0, z0), 0.0, 1.0);
+    float v1 = 1.0 - clamp(min(x1, z1), 0.0, 1.0);
+    if (v0 > 0.1) {
+        gl_FragColor = mix(gl_FragColor, gridColor, v0);
+    }
+    else {
+        gl_FragColor = mix(gl_FragColor, gridColor2, v1);
+    }
+
+    vec3 diffuseColor = vec3(0.0, 0.0, 0.0);
+
+#ifdef AMBIENT_LIGHT_COUNT
+    for(int _idx_ = 0; _idx_ < AMBIENT_LIGHT_COUNT; _idx_++)
+    {
+        diffuseColor += ambientLightColor[_idx_];
+    }
+#endif
+#ifdef AMBIENT_SH_LIGHT_COUNT
+    for(int _idx_ = 0; _idx_ < AMBIENT_SH_LIGHT_COUNT; _idx_++)
+    {{
+        diffuseColor += calcAmbientSHLight(_idx_, v_Normal) * ambientSHLightColor[_idx_];
+    }}
+#endif
+
+#ifdef DIRECTIONAL_LIGHT_COUNT
+#if defined(DIRECTIONAL_LIGHT_SHADOWMAP_COUNT)
+    float shadowContribsDir[DIRECTIONAL_LIGHT_COUNT];
+    if(shadowEnabled)
+    {
+        computeShadowOfDirectionalLights(v_WorldPosition, shadowContribsDir);
+    }
+#endif
+    for(int i = 0; i < DIRECTIONAL_LIGHT_COUNT; i++)
+    {
+        vec3 lightDirection = -directionalLightDirection[i];
+        vec3 lightColor = directionalLightColor[i];
+
+        float ndl = dot(v_Normal, normalize(lightDirection));
+
+        float shadowContrib = 1.0;
+#if defined(DIRECTIONAL_LIGHT_SHADOWMAP_COUNT)
+        if( shadowEnabled )
+        {
+            shadowContrib = shadowContribsDir[i];
+        }
+#endif
+
+        diffuseColor += lightColor * clamp(ndl, 0.0, 1.0) * shadowContrib;
+    }
+#endif
+
+#ifdef SSAOMAP_ENABLED
+    diffuseColor *= texture2D(ssaoMap, (gl_FragCoord.xy - viewport.xy) / viewport.zw).r;
+#endif
+
+    gl_FragColor.rgb *= diffuseColor;
+
+    gl_FragColor.a *= 1.0 - clamp(length(v_WorldPosition.xz) / 40.0, 0.0, 1.0);
+
+}
+
+@end
+`);
+
+/// Init background
+clay.application.create('#background', {
+
+    graphic: {
+        tonemapping: true,
+        linear: true,
+        shadow: true
+    },
+
+    init: function (app) {
+
+        // Create camera
+        this._cameraRoot = app.createNode();
+        this._cameraRoot.add(app.createCamera([0, 3, 15], [0, 2, 0]));
+
+        // Create light
+        app.createDirectionalLight([-1, -3, -1]);
+        app.scene.add(new clay.light.AmbientSH({
+            intensity: 0.6,
+            coefficients: [0.844, 0.712, 0.691, -0.037, 0.083, 0.167, 0.343, 0.288, 0.299, -0.041, -0.021, -0.009, -0.003, -0.041, -0.064, -0.011, -0.007, -0.004, -0.031, 0.034, 0.081, -0.060, -0.049, -0.060, 0.046, 0.056, 0.050]
+        }));
+
+        app.container.addEventListener('mousemove', (e) => {
+            this._moveX = e.clientX - window.innerWidth / 2;
+            this._moveY = e.clientY - window.innerHeight / 2;
+        });
+
+        this._initGround(app);
+
+        return app.loadModel('./assets/logo/logo.gltf').then((result) => {
+            this._rootNode = result.rootNode;
+        });
+    },
+
+    _initGround: function (app) {
+        var ground = app.createPlane({
+            shader: new clay.Shader(clay.Shader.source('clay.ground.vertex'), clay.Shader.source('clay.ground.fragment'))
+        });
+        ground.castShadow = false;
+        ground.rotation.rotateX(-Math.PI / 2);
+        ground.scale.set(50, 50, 1);
+    },
+
+    loop: function () {
+        this._cameraRoot.rotation.identity()
+            .rotateY(-this._moveX / window.innerHeight / 2)
+            .rotateX(-this._moveY / window.innerWidth / 2);
+    }
+});
